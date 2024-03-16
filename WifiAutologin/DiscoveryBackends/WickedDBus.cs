a290bb5ee7f63ea45b2145f12a55fb266362e652
Alexander "Ace" Olofsson <ace@haxalot.com>
2024-03-16 12:19:25 +0100

    Add a stub for SUSE Wicked

---
+++ WifiAutologin/DiscoveryBackends/WickedDBus.cs
@@ -0,0 +1,40 @@
+#if OS == UNIX
+// using Wicked.DBus;
+using Tmds.DBus;
+#endif
+
+using WifiAutologin.Util;
+
+namespace WifiAutologin.DiscoveryBackends;
+
+[DiscoveryBackend("Wicked", OSes = OSFamily.Linux)]
+public class WickedDBus : IDiscoveryBackend, IDisposable
+{
+    static ILogger Logger { get; } = WifiAutologin.Logger.Global[typeof(WickedDBus)];
+
+    public void Dispose()
+    {
+    }
+
+    public WickedDBus()
+    {
+    }
+
+    public bool IsAvailable =>
+#if OS == UNIX
+        false &&
+        OperatingSystem.IsLinux();
+#else
+        false;
+#endif
+    public bool IsConnected => ConnectedNetworks.Any();
+    public bool IsConnectedToVPN => false;
+    public bool SupportsDaemonize => true;
+
+    public IDisposable WatchChanges(Action<IDiscoveryBackend> handler)
+    {
+        return new DisposableWrapper();
+    }
+
+    public IEnumerable<string> ConnectedNetworks { get { return new string[0]; } }
+}
